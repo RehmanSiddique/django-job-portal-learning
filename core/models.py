ba76d9196cf8d3ff2112a4b3eba0f9de5ef75f01
Rehman Siddique <you@example.com>
2026-02-18 10:43:16 -0700

    Update Models and add new admin

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
@@ -15,13 +18,21 @@ class Job(models.Model):
     company = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
+    create_by= models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title   
 class Apply(models.Model):
+    Status_CHOICES = [
+        ('applied', 'Applied'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    applied_at = models.DateTimeField(auto_now_add=True)
+    status= models.CharField(max_length=100, choices=Status_CHOICES, default='applied')
+    
+    applied_date= models.DateTimeField(auto_now_add=True, editable=False)
     
 
     def __str__(self):
